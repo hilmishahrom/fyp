5c3ec605552ac5f1a9ccccb70c6b82d170a1522f
hilmishahrom <68212535+hilmishahrom@users.noreply.github.com>
2020-07-13 12:44:43 +0800

    Add files via upload

---
+++ Web System/includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+            <div class="footer-area">
+                <p>Parking Management System @ 2020. All right reserved. Hilmi Inc.</p>
+            </div>
+        </footer>
---
+++ Web System/includes/sidebar.php
@@ -0,0 +1,60 @@
+   <div class="sidebar-menu">
+            <div class="sidebar-header">
+                <div class="logo">
+                    <p style="color: white">PMS ADMIN</p>
+                </div>
+            </div>
+            <div class="main-menu">
+                <div class="menu-inner">
+                    <nav>
+                        <ul class="metismenu" id="menu">
+                            <li class="active">
+                                <a href="dashboard.php" aria-expanded="true"><i class="ti-dashboard"></i><span>Dashboard</span></a>
+                               
+                            </li>
+<!-- 
+                            <li>
+                                <a href="manage-ticket.php" aria-expanded="true"><i class="ti-layout-sidebar-left"></i><span>Manage Type Ticket
+                                    </span></a>
+                               s
+                            </li> -->
+                          
+                            <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-folder"></i><span>Normal Ticket</span></a>
+                                <ul class="collapse">
+                                    <li><a href="add-normal-ticket.php">Add Ticket</a></li>
+                                    <li><a href="add-ticket-in-use.php">Add Ticket In-Use</a></li>
+                                    <li><a href="manage-normal-ticket.php">Manage Ticket</a></li>
+                                </ul>
+                            </li>
+                             <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-folder"></i><span>Monthly Users Ticket</span></a>
+                                <ul class="collapse">
+                                    <li><a href="add-monthly-ticket.php">Add Ticket</a></li>
+                                    <li><a href="view2-monthly-ticket.php">Edit Ticket</a></li>
+                                    <li><a href="add-mthly-ticket-in-use.php">Add Ticket In-Use</a></li>
+                                    <li><a href="manage-monthly-ticket.php">Manage Ticket</a></li>
+                                </ul>
+                            </li>
+                           
+                          <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-folder"></i><span>Location</span></a>
+                                <ul class="collapse">
+                                    <li><a href="add-location.php">Add Location</a></li>
+                                </ul>
+                            </li>
+                            <!-- <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-search"></i><span>Search</span></a>
+                                <ul class="collapse">
+                                    <li><a href="normal-search.php">Normal Ticket Search</a></li>
+                                    <li><a href="foreigner-search.php">Foreigner Ticket Search</a></li>
+                                </ul>
+                            </li> -->
+                            
+                           
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        
