25a43057d909074912755397d1856404a41344e5
hilmishahrom <68212535+hilmishahrom@users.noreply.github.com>
2020-07-13 12:44:15 +0800

    Update dbconnection.php

--- Web System/includes/dbconnection.php
+++ Web System/includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("192.168.64.2", "admin", "1234", "ptmsdb");
+$con=mysqli_connect("localhost", "admin", "", "ptmsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
